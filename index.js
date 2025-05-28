3a6c07e463328b576b93d9658829851150e8ee63
yuga1000 <webkarma.3.0@gmail.com>
2025-05-28 14:40:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const { fetchAndStoreDumps } = require('./fetcher');
 const { extractFromAllFiles } = require('./extractor');
+const { sendTelegramMessage } = require('./telegram');
 
 async function main() {
   await fetchAndStoreDumps();
 
   const keys = extractFromAllFiles();
   if (keys.length > 0) {
-    console.log(`✨ Найдено всего ключей: ${keys.length}`);
+    console.log(`🔑 Найдено всего ключей: ${keys.length}`);
     for (const key of keys) {
-      console.log(`🧷 ${key}`);
+      console.log(`> ${key}`);
     }
+    await sendTelegramMessage(`✅ Найдено ключей: ${keys.length}`);
   } else {
-    console.log('🕳️ Ключи не найдены.');
+    console.log('⚠️ Ключи не найдены.');
+    await sendTelegramMessage('⚠️ Ключи не найдены.');
   }
 }
 
