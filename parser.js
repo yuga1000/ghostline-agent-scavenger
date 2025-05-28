f758b429461eb03ebb54e0e33feb0e245ec8afe2
yuga1000 <webkarma.3.0@gmail.com>
2025-05-28 11:29:01 +0800

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,1 +1,11 @@
+export function parseDumpFile(text) {
+  const lines = text.split('\n');
+  const keys = [];
 
+  for (const line of lines) {
+    const match = line.match(/\b[a-f0-9]{64}\b/i);
+    if (match) keys.push(match[0]);
+  }
+
+  return keys;
+}
