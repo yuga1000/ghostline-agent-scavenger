47d6e3ff2e9dd2a1e73dbfcd4da4dcbd4d7d3158
yuga1000 <webkarma.3.0@gmail.com>
2025-05-28 14:44:53 +0800

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
-const token = process.env.TELEGRAM_TOKEN;
-const chatId = process.env.TELEGRAM_CHAT_ID;
+const token = process.env.BOT_TOKEN;
+const chatId = process.env.CHAT_ID;
 
 async function sendTelegramMessage(message) {
   if (!token || !chatId) {
