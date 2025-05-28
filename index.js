290d3ef3f237b4623de456afc6e7bcace74e1970
yuga1000 <webkarma.3.0@gmail.com>
2025-05-28 13:32:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-const parser = require('./parser');
-const { sendToTelegram } = require('./telegram');
+const { fetchAndStoreDumps } = require('./fetcher');
 
-const dumpPath = path.join(__dirname, 'sample_dump.txt');
-
-function scanDump() {
-  const lines = fs.readFileSync(dumpPath, 'utf-8').split('\n').filter(Boolean);
-  for (const line of lines) {
-    const result = parser.parseLine(line.trim());
-    if (result) {
-      sendToTelegram(result);
-    }
-  }
-}
-
-scanDump();
+(async () => {
+  await fetchAndStoreDumps();
+})();
