1b01716db7de37121faed8a969bc4c5103db8432
yuga1000 <webkarma.3.0@gmail.com>
2025-05-28 13:25:33 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,2 +1,10 @@
-// пока пустой — можно будет сюда выносить общие функции
-module.exports = {};
+const fs = require('fs');
+const path = require('path');
+
+function saveTextToFile(name, content) {
+  const filePath = path.join(__dirname, 'dumps', name);
+  fs.mkdirSync(path.dirname(filePath), { recursive: true });
+  fs.writeFileSync(filePath, content);
+}
+
+module.exports = { saveTextToFile };
