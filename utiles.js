1a397ce8fd6ab6b844ada98516a1831e89d27d0d
yuga1000 <webkarma.3.0@gmail.com>
2025-05-28 13:17:51 +0800

    Update utiles.js

--- utiles.js
+++ utiles.js
@@ -1,18 +1,10 @@
-import Web3 from 'web3';
+const fs = require('fs');
+const path = require('path');
 
-const web3 = new Web3(process.env.ETH_RPC_URL);
-
-export async function checkKey(privateKey) {
-  try {
-    const account = web3.eth.accounts.privateKeyToAccount('0x' + privateKey);
-    const balanceWei = await web3.eth.getBalance(account.address);
-    const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
-
-    return {
-      address: account.address,
-      balance: parseFloat(balanceEth),
-    };
-  } catch (e) {
-    return null;
-  }
+function saveTextToFile(name, content) {
+  const filePath = path.join(__dirname, 'dumps', name);
+  fs.mkdirSync(path.dirname(filePath), { recursive: true });
+  fs.writeFileSync(filePath, content);
 }
+
+module.exports = { saveTextToFile };
