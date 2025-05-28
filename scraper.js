399623276aa26e938c2e0068af4b89a5e838840d
yuga1000 <webkarma.3.0@gmail.com>
2025-05-28 16:32:31 +0800

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -2,8 +2,8 @@
 
 async function fetchPasteLinks() {
   return [
-    'https://pastebin.com/raw/your_real_paste' // ← сюда вставь рабочую raw-ссылку вручную
-  ];
+  'https://pastebin.com/raw/1A2b3C4D'
+];
 }
 
 module.exports = {
